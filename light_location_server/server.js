70fc7d7b3ebede56afec7aa9eef0aaedc5ac7608
utting <bm.utting@gmail.com>
2016-11-19 21:15:15 +1000

    Reduced colors to basic six and corrected color merging.

--- light_location_server/server.js
+++ light_location_server/server.js
@@ -1,5 +1,6 @@
 // Library imports
 var colors 				= 	require("colors");
+var http                = require("http");
 var webSocketServer 	= 	require("ws").Server;
 
 // Application imports
@@ -25,6 +26,12 @@ var defaults 			= 	{
 								updateFreq: 250
 							}
 
+// dictionary that maps each device to its assigned color
+var color_dict = {};
+
+// steps through the colors for each new device
+var next_color = 0;
+
 /* _updateFixtureColor
  * Updates local in-memory collection of fixture and their current colour
  * fixture_id: byte, the start channel for the fixture corresponding with the fixture_id in fixtures.js
@@ -32,12 +39,57 @@ var defaults 			= 	{
  */ 
 
 // TODO: Add hook to onDataChange from tracking service
+var url = "http://localhost:8080/getalldevices";
+
+http.get(url, _onConnectionToPositionService).on("error", _onConnectionErrorToPositionService);
+
+function _merge_colors(devices) {
+    var result = { r:0, g:0, b:0 };
+    for (var dev of devices) {
+	if (!(dev.name in color_dict)) {
+	    var c = palette[next_color % palette.length];
+	    color_dict[dev.name] = { r:c[0], g:c[1], b:c[2] };
+	    console.log("ASSIGNED COLOR ", c, " TO ", dev.name);
+	    next_color += 1;
+	}
+	var c = color_dict[dev.name];
+	result.r += c.r;
+	result.g += c.g;
+	result.b += c.b;
+    }
+    result.r = Math.min(result.r, 255);
+    result.g = Math.min(result.g, 255);
+    result.b = Math.min(result.b, 255);
+    console.log("RESULT COLOR", result.r, result.g, result.b);
+    return result;
+}
+
+function _onConnectionToPositionService (res) {
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var fixtures  = JSON.parse(body);
+	for (var fixture of fixtures) {
+	    if (fixture.devices.length > 0) {
+		// merge colors here
+		merged_color = _merge_colors(fixture.devices);
+		_updateFixtureColor(fixture.fixture_id, merged_color);
+	    }
+	}
+    });
+}
+
+function _onConnectionErrorToPositionService (e) {
+    console.log("CONNECTION ERROR".red.bold, e);
+}
 
 function _updateFixtureColor (id, color) {
 	var fixture = fixtures.filter(fixture => fixture.fixture_id === id);
 	fixture.color = color;
-
-
 	console.log("UPDATED:".white.bold, fixture);
 }
 
